a1cedecc657f21b725bc20bfd760c267c2bf7744
Marios Antonoudiou <marios.antonoudiou@gmail.com>
2018-10-04 18:35:01 +0300

    Initial

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const domContainer = document.querySelector('#app');
+
+const App = () => <p>This is a placeholder</p>;
+
+ReactDOM.render(<App />, domContainer);
