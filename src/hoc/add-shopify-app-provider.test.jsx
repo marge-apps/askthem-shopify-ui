fd0103e8ee739ce24232e956013c771a59fdf6cb
Marios Antonoudiou <marios.antonoudiou@gmail.com>
2018-10-10 08:54:07 +0300

    Add hoc for shopify app provider

---
+++ src/hoc/add-shopify-app-provider.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import testRenderer from 'react-test-renderer'
+import addShopifyAppProvider from './add-shopify-app-provider'
+
+describe('addShopifyAppProvider hoc', () => {
+    test('renders component properly', () => {
+        const Component = addShopifyAppProvider(() => <div>Hello world!</div>)
+
+        const renderer = testRenderer.create(<Component />)
+
+        const actual = renderer.toJSON()
+        const expectation = { type: 'div', props: {}, children: [ 'Hello world!' ] }
+
+        expect(actual).toEqual(expectation)
+    })
+})
