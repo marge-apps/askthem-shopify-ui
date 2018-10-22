6495d03bd09c86d66c89d900255066e5dd57b8b2
Marios Antonoudiou <marios.antonoudiou@gmail.com>
2018-10-22 17:43:11 +0300

    Add settings

--- src/screens/settings.jsx
+++ src/screens/settings.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-import {Card, Layout, Page, FooterHelp, FormLayout, Checkbox, TextField, DropZone} from '@shopify/polaris';
+import {Banner, Card, Layout, Page, FooterHelp, FormLayout, Checkbox, TextField, DropZone} from '@shopify/polaris';
 
 const View = props => (
     <Page
@@ -13,6 +13,16 @@ const View = props => (
 		]}
         >
         <Layout>
+        <Layout.Section>
+            <Banner
+                title="This application is under development"
+                status="info"
+                >
+                <p>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque orci augue, vel rhoncus lacus condimentum non. Aliquam dolor ex, finibus a odio et, sollicitudin volutpat quam. Pellentesque tempor viverra est eget sagittis. Suspendisse pellentesque, lacus a commodo mattis, lorem mi fermentum risus, volutpat semper orci elit et lacus. Nullam hendrerit lacus a mauris aliquam, in tincidunt metus vulputate. Quisque ac ipsum consectetur, pellentesque nulla eu, hendrerit enim. Cras nec gravida justo. Donec venenatis auctor sodales. Fusce eget libero consequat, commodo tortor nec, hendrerit neque. Nulla velit magna, accumsan at tempor vel, lobortis sed justo. Sed tristique tristique augue, quis malesuada lectus vestibulum sit amet. Proin nec feugiat urna, sit amet sodales urna. </p>
+            </Banner>
+        </Layout.Section>
         <Layout.AnnotatedSection
             title="Basic settings"
             description="AskThem will use these settings to contact your customers on your behalf."
